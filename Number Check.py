1a8e47c4e06a8d24bb38cee74d0bf48200089641
Machuki Clinton <48854561+Machukichuxx@users.noreply.github.com>
2020-11-03 20:08:54 +0300

    Add files via upload

---
+++ Number Check.py
@@ -0,0 +1,9 @@
+Write a Python program to check if a number is even or odd .
+
+number = int(input("Input a number "))
+Determiner = number % 2
+
+if Determiner > 0:
+print(number,"is an odd number")
+else:
+print(number,"is an even number.")
